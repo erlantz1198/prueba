e976501128f1c7f75caff11781d57ba01af32cfb
Erlantz <erlantzmarcos@opendeusto.es>
2016-10-18 13:40:17 +0200

    He añadido mi primer fichero

---
+++ numero = random.py
@@ -0,0 +1,20 @@
+#import random as r
+
+#numero = r.randint(0, 100)
+
+#print(numero)
+
+
+#from random import randint, random
+
+#numero = randint(0,100)
+#print(numero)
+
+def main():
+	import funciones
+
+	resultado = funciones.sumar(3, 5)
+	print (resultado)
+
+if __name__ == '__main__'
+	main()
